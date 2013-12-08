ad34cffc3ec3558d71ae53f6344a2b203946325b
Andrew B <andrewburch79@gmail.com>
2013-12-08 16:12:51 +1100

    Fixed more indentation issues

--- clear/clearscreen3.asm
+++ clear/clearscreen3.asm
@@ -5,15 +5,15 @@
 ;		screen quickly
 ;
 
-		.org $c000		; begin (49152)
+        .org $c000      ; begin (49152)
 
-		lda #$20		; space character
-		ldx #00
-loop	sta $0400, x	; write 4 bytes per loop
-		sta $04fa, x
-		sta $05f4, x
-		sta $06ee, x
-		inx
-		cpx #$fa
-		bne loop
-		rts				; return to BASIC
+        lda #$20        ; space character
+        ldx #00
+loop    sta $0400, x    ; write 4 bytes per loop
+        sta $04fa, x
+        sta $05f4, x
+        sta $06ee, x
+        inx
+        cpx #$fa
+        bne loop
+        rts             ; return to BASIC
--- raster/widescreen.asm
+++ raster/widescreen.asm
@@ -7,64 +7,64 @@
 ;		of wide screen
 ;
 
-		.org $c000		; begin (49152)
+        .org $c000      ; begin (49152)
 
-		jsr $e544		; clear screen
-		lda #00			; black background
-		sta $d021
+        jsr $e544       ; clear screen
+        lda #00         ; black background
+        sta $d021
 
-		sei				; set up interrupt
-		lda #$7f
-		sta $dc0d		; turn off the CIA interrupts
-		sta $dd0d
-		and $d011		; clear high bit of raster line
-		sta $d011		
+        sei             ; set up interrupt
+        lda #$7f
+        sta $dc0d       ; turn off the CIA interrupts
+        sta $dd0d
+        and $d011       ; clear high bit of raster line
+        sta $d011		
 
-		ldy #50			; trigger on first screen line
-		sty $d012
+        ldy #50         ; trigger on first screen line
+        sty $d012
 
-		lda #<widetop	; load interrupt address
-		ldx #>widetop
-		sta $0314
-		stx $0315
+        lda #<widetop   ; load interrupt address
+        ldx #>widetop
+        sta $0314
+        stx $0315
 
-		lda #$01 		; enable raster interrupts
-		sta $d01a
-		cli
-		rts				; back to BASIC
+        lda #$01        ; enable raster interrupts
+        sta $d01a
+        cli
+        rts             ; back to BASIC
 
-widetop	jsr latch
-		lda #00			; set border colour
-		sta $d020
-		sta $d021
+widetop jsr latch
+        lda #00         ; set border colour
+        sta $d020
+        sta $d021
 
-		lda #<widebot	; point to next interrupt
-		ldx #>widebot
-		sta $0314
-		stx $0315
+        lda #<widebot   ; point to next interrupt
+        ldx #>widebot
+        sta $0314
+        stx $0315
 
-		lda #250		; set trigger last screen line
-		sta $d012
+        lda #250        ; set trigger last screen line
+        sta $d012
 
-		asl $d019		; acknowledge interrupt
-		jmp $ea81
+        asl $d019       ; acknowledge interrupt
+        jmp $ea81
 
-widebot	jsr latch
-		lda #06			; set border colour
-		sta $d020
+widebot jsr latch
+        lda #06         ; set border colour
+        sta $d020
 
-		lda #<widetop	; point to next interrupt
-		ldx #>widetop
-		sta $0314
-		stx $0315
+        lda #<widetop   ; point to next interrupt
+        ldx #>widetop
+        sta $0314
+        stx $0315
 
-		ldy #50			; set trigger scan line
-		sty $d012
+        ldy #50         ; set trigger scan line
+        sty $d012
 
-		asl $d019		; acknowledge interrupt
-		jmp $ea31
+        asl $d019       ; acknowledge interrupt
+        jmp $ea31
 
-latch	ldx #02			; stable raster delay
-		dex
-		bne latch+2
-		rts
+latch   ldx #02         ; stable raster delay
+        dex 
+        bne latch+2
+        rts
